7ece9bd9cf582a5c9ef4e8fc901fbbc180221f75
olipliche <olipliche@gmail.com>
2021-10-12 10:40:01 +0100

    Add header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header-section">
+    <div className="header-logo">
+      <h3>Space Traveler's Hub</h3>
+    </div>
+    <nav className="header-nav">
+      <ul className="header-nav_ul">
+        <li className="Rockets">Nav Under Construction</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
