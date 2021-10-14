e0b7213e38995c223441c6054291e7e058925cb3
lily-coder <lilyondie@gmail.com>
2021-10-14 17:22:15 +0300

    Add rocket reservation and cancellation of reservation

--- src/pages/RocketsCards.js
+++ src/pages/RocketsCards.js
@@ -1,7 +1,11 @@
 /* eslint no-unused-vars : 'off' */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getRocket } from '../redux/rockets/rockets.js';
+import {
+  getRocket,
+  reserveRocket,
+  unreserveRocket,
+} from '../redux/rockets/rockets.js';
 
 const RocketsCards = () => {
   const dispatch = useDispatch();
@@ -20,12 +24,21 @@ const RocketsCards = () => {
     }
   }, []);
 
+  const reserveRockets = (e) => {
+    dispatch(reserveRocket(e.target.id));
+  };
+
+  const unReserveRockets = (e) => {
+    dispatch(unreserveRocket(e.target.id));
+  };
+
   return (
     <div className='rocket-card'>
       {myRocketArray.map((rocket) => (
         <div key={rocket.id} className='rockets-div'>
           <div className='rocket-imgs'>
-            <img src={rocket.flickr_images}
+            <img
+              src={rocket.flickr_images}
               alt={rocket.rocket_name}
               width='300'
               height='200'
@@ -36,9 +49,28 @@ const RocketsCards = () => {
           <div className='rockets-desc'>
             <h3 className='rockets-desc-title'>{rocket.rocket_name}</h3>
             <p className='rockets-desc-p'>
+              {rocket.reserved ? <button className='reserved-span'>Reserved</button> : null}
               {rocket.description}
             </p>
-            <button className='rockets-desc-btn'>Reserve Rocket</button>
+            {rocket.reserved ? (
+              <button
+                type='button'
+                onClick={unReserveRockets}
+                id={rocket.id}
+                className='unreserve-btn'
+              >
+                Cancel Reservation
+              </button>
+            ) : (
+              <button
+              type='button'
+              className='rockets-desc-btn'
+              onClick={reserveRockets}
+              id={rocket.id}
+            >
+              Reserve Rocket
+            </button>
+            )}
           </div>
         </div>
       ))}
