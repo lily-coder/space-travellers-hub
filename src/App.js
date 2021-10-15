0fcc8f0983043a2833ac38618cce738c8ce70241
olipliche <olipliche@gmail.com>
2021-10-15 16:33:05 +0100

    Update mission files

--- src/App.js
+++ src/App.js
@@ -9,17 +9,19 @@ import Header from './components/Header.js';
 import Rockets from './components/Rockets.js';
 import Missions from './components/Missions.js';
 import Profile from './components/Profile.js';
+import MissionsList from './pages/MissionsList.js';
 
 const App = () => (
   <Router>
     <div className='app'>
       <Header />
+      <Missions />
       <Switch>
         <Route exact path='/'>
           <Rockets />
         </Route>
         <Route path='/Missions'>
-        <Missions />
+          <MissionsList />
         </Route>
         <Route path='/Profile'>
           <Profile />
